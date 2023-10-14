b998a9dbfb00bf352a496a8864333448cca7c95d
Szatan-K <kacperszatan03@gmail.com>
2023-10-14 12:02:47 +0200

    daily

---
+++ LeetCodeCS/1.Two-Sum/solution.cs
@@ -0,0 +1,14 @@
+using System;
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < (nums.Length - 1); i++)
+        {
+            for (int j = i+1; j < nums.Length; j++)
+            {
+                if (target - nums[i] == nums[j])
+                return new int[]{i, j};
+            }
+        }
+        return new int[] {0,0};
+    }
+}
