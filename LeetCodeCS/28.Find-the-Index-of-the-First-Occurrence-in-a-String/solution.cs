2fc489806be75b095173a42cafd993689509e827
Szatan-K <kacperszatan03@gmail.com>
2023-10-22 14:15:55 +0200

    first attempt

---
+++ LeetCodeCS/28.Find-the-Index-of-the-First-Occurrence-in-a-String/solution.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        int maxi = haystack.Length - needle.Length + 1;
+        for(int i = 0; i < maxi; i++)
+        {   
+            Console.WriteLine(haystack[i] + " : " + needle);
+            string res = haystack.Substring(i, needle.Length);
+            if (string.Equals(res, needle))
+                return i;
+        }
+        return -1;
+    }
+}
